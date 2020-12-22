e5f60b44d5763026927d483b6b0aff80e7dd162b
RehanImran <66786797+RehanImran@users.noreply.github.com>
2020-12-22 23:49:30 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
-//import HomeStack from "./Navigation/HomeStack";
-//import Login from './Components/Login';
-import EnterRecord from "./Components/EnterRecord";
+import React from "react";
+//import { StyleSheet, Text, View } from "react-native";
+import HomeStack from "./Navigation/HomeStack";
+//import Login from "./Components/Login";
+//import EnterRecord from "./Components/EnterRecord";
 //import RunNode from './Node/RunNode';
-//import Upload from './Components/Upload'
+//import Upload from "./Components/Upload";
 //import Test from './Database/test'
 //import Test from "./Components/test";
-
-import Home from "./Components/Home";
+//import Home from "./Components/Home";
+//import Forward from "./Components/ForwardDocument";
+import HyperText from "./Test/HyperText";
 
 export default function App() {
   return (
     //<RunNode/>
+    //<Forward />
     //<Test />
-    //<HomeStack />
+    <HomeStack />
+    //<HyperText/>
     //<Home />
-    //<Login/>
-    <EnterRecord />
-    //<Upload/>
+    // <Login />
+    //<EnterRecord />
+    //<Upload />
     //<Test/>
   );
 }
